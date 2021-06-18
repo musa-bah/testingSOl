c51f498072f60738f3a8e62f2c088c315e116ac0
Musa Bah <Musa.Bah@BNSF.com>
2021-06-18 08:59:29 -0600

    testing file structure

---
+++ Apps/AppOne/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
